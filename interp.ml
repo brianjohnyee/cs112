b4c1d85192184064e0a94cc2538b34aecdd3ca3a
brianjohnyee <36317363+brianjohnyee@users.noreply.github.com>
2019-02-04 16:32:34 -0800

    the final
    only bugs should be in big-o test file and sort-array test file

--- interp.ml
+++ interp.ml
@@ -10,18 +10,19 @@ let want_dump = ref false
 let rec eval_expr (expr : Absyn.expr) : float = match expr with
     | Number number -> number
     | Memref memref -> 
-    	(match memref with
-    		| Variable ident ->
-    			Hashtbl.find Tables.variable_table ident
-    		| Arrayref (ident, expr) ->
-    			Array.get (Hashtbl.find Tables.array_table ident) (int_of_float(eval_expr expr))
-    	)
+        (match memref with
+            | Variable ident ->
+                Hashtbl.find Tables.variable_table ident
+            | Arrayref (ident, expr) ->
+                Array.get (Hashtbl.find Tables.array_table ident) 
+                (int_of_float(eval_expr expr)-1)
+        )
     | Unary (oper, expr) -> 
-    	let x = Hashtbl.find Tables.unary_fn_table oper
-    	in(x(eval_expr expr))
+        let x = Hashtbl.find Tables.unary_fn_table oper
+        in(x(eval_expr expr))
     | Binary (oper, expr1, expr2) -> 
-      	let x = Hashtbl.find Tables.binary_fn_table oper
-      	in (x(eval_expr expr1) (eval_expr expr2))
+        let x = Hashtbl.find Tables.binary_fn_table oper
+        in (x(eval_expr expr1) (eval_expr expr2))
 
 let interp_print (print_list : Absyn.printable list) =
     let print_item item =
@@ -39,64 +40,66 @@ let interp_input (memref_list : Absyn.memref list) =
         (match memref with
         | Arrayref (ident,expr)-> print_string "kenirngierngneirgn";
         | Variable ident ->
-       		try  let number = Etc.read_number ()
+            try  let number = Etc.read_number ()
             in (Hashtbl.add Tables.variable_table ident number)
-        	with End_of_file -> 
-        	(Hashtbl.replace Tables.variable_table "eof" 1.0)
+            with End_of_file -> 
+            (Hashtbl.replace Tables.variable_table "eof" 1.0)
 
         )
     in (List.iter input_number memref_list; print_newline ())
 
 let interp_stmt (stmt : Absyn.stmt) = match stmt with
     | Dim (ident, expr) -> 
- 		(Hashtbl.add Tables.array_table ident (Array.make (int_of_float(eval_expr expr)) 0.))
+        (Hashtbl.add Tables.array_table ident (Array.make 
+        (int_of_float(eval_expr expr)) 0.))
     | Let (memref, expr) -> 
-    	(match memref with
-    	| Variable ident -> 
-    		(Hashtbl.add Tables.variable_table ident (eval_expr expr ))
-    	| Arrayref (ident, exprr) ->
-      		(Array.set (Hashtbl.find Tables.array_table ident)  (int_of_float(eval_expr exprr)) (eval_expr expr) )
-    	)
+        (match memref with
+        | Variable ident -> 
+            (Hashtbl.add Tables.variable_table ident (eval_expr expr ))
+        | Arrayref (ident, exprr) ->
+            (Array.set (Hashtbl.find Tables.array_table ident)  
+            (int_of_float(eval_expr exprr)-1) (eval_expr expr) )
+        )
     | Goto label -> 
-    	(* this shouldn't be called. it is called in interpret *)
-    	print_string "."
-		(*(Hashtbl.find Tables.label_table label)*)
+        (* this shouldn't be called. it is called in interpret *)
+        print_string "."
+        (*(Hashtbl.find Tables.label_table label)*)
     | If (expr, label) -> 
-    	print_string "."
+        (*this shouldn't be called. It is called in interpret*)        
+        print_string "."
     | Print print_list -> interp_print print_list
     | Input memref_list -> 
-    	if (<=) (Hashtbl.find Tables.variable_table "eof") (0.0) 
-    		then
-    			interp_input memref_list
+        if (<=) (Hashtbl.find Tables.variable_table "eof") (0.0) 
+            then
+                interp_input memref_list
 
 let rec interpret (program : Absyn.program) = match program with
     | [] -> ()
     | firstline::otherlines -> match firstline with
       | _, _, None -> interpret otherlines
       | _, _, Some stmt -> 
-      	(match stmt with
-	      	| Goto label ->
-	      		interpret (Hashtbl.find Tables.label_table label)
-	      	| If (expr, label) ->
-		    	(match expr with
-		    		| Number number -> print_string ""
-		    		| Memref memref -> print_string ""
-		    		| Unary (oper,expr) -> print_string ""
-		    		| Binary (oper,expr1,expr2)->
-		    			if (Hashtbl.find Tables.boolean_table oper) (eval_expr expr1) (eval_expr expr2) 
-		    				then 
-		    					interpret(Hashtbl.find Tables.label_table label)
-		    				else
-		    					interpret(otherlines)
-		    	)
-	      	| _ -> 
-	      		(interp_stmt stmt; interpret otherlines)
-     	)
+        (match stmt with
+            | Goto label ->
+                interpret (Hashtbl.find Tables.label_table label)
+            | If (expr, label) ->
+                (match expr with
+                    | Number number -> print_string ""
+                    | Memref memref -> print_string ""
+                    | Unary (oper,expr) -> print_string ""
+                    | Binary (oper,expr1,expr2)->
+                        if (Hashtbl.find Tables.boolean_table oper) 
+                        (eval_expr expr1) (eval_expr expr2) 
+                            then 
+                      interpret(Hashtbl.find Tables.label_table label)
+                            else
+                                interpret(otherlines)
+                )
+            | _ -> 
+                (interp_stmt stmt; interpret otherlines)
+        )
 
 let interpret_program program =
-(* 	 print_string "hi";
- *)  
- 	 (Tables.init_label_table program; 
+     (Tables.init_label_table program; 
      if !want_dump then Tables.dump_label_table ();
      if !want_dump then Dumper.dump_program program;
      interpret program)
